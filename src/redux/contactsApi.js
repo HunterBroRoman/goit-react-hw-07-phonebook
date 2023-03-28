b7c874c660d8e57a91c26d322fa7151f4ef5a4ff
Roman <sayga76239@gmail.com>
2023-03-28 14:22:24 +0200

    komment_contactApi

--- src/redux/contactsApi.js
+++ src/redux/contactsApi.js
@@ -1,11 +1,11 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const contactsApi = createApi({
-  reducerPath: 'contacts',
+  reducerPath: 'contacts', // указывает путь к редьюсеру, который будет использоваться для хранения данных, связанных с этим API.
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://62ec100d818ab252b6f765e1.mockapi.io/api/v1',
-  }),
-  tagTypes: ['Contact'],
+  }), //указывает базовый запрос, который будет использоваться для отправки запросов к API. В данном случае используется fetchBaseQuery для отправки запросов с помощью стандартного метода Fetch API. Также задан baseUrl, который будет использоваться для всех запросов.
+  tagTypes: ['Contact'],//задает типы тегов, которые будут использоваться для инвалидации кэша. В данном случае используется один тип тега - "Contact".
   endpoints: builder => ({
     getContacts: builder.query({
       query: () => `/contacts`,
